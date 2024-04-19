66740248be5021c13c9f1e6f8c52831f50d584f1
willamessantos <willamessantos@live.com>
2024-04-19 00:17:03 -0300

    refactor(home page) products data fetch

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,15 +1,25 @@
+import { api } from "@/data/api";
+import { Product } from "@/data/types/product";
 import Image from "next/image";
 import Link from "next/link";
 
+async function getProductsFeatured(): Promise<Product[]> {
+  const response = await api("/products/featured");
+  const products = await response.json();
+  return products;
+}
+
 export default async function Home() {
+  const [highlightedProduct, ...otherProducts] = await getProductsFeatured();
+
   return (
     <div className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
       <Link
-        href="/"
+        href={`/product/${highlightedProduct.slug}`}
         className="group relative col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden flex justify-center"
       >
         <Image
-          src="/pngfind.com-camisetas-png-5353213.png"
+          src={highlightedProduct.image}
           className="group-hover:scale-105 transition-transform duration-500"
           width={700}
           height={700}
@@ -19,61 +29,51 @@ export default async function Home() {
 
         <div
           className="absolute bottom-28 right-28 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5"
-          title="Camiseta Nike Algodão Vermelha"
+          title={highlightedProduct.title}
         >
-          <span className="text-sm truncate">
-            Camiseta Nike Algodão Vermelha
-          </span>
+          <span className="text-sm truncate">{highlightedProduct.title}</span>
           <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-            R$129
+            {highlightedProduct.price.toLocaleString("pt-br", {
+              style: "currency",
+              currency: "BRL",
+              minimumFractionDigits: 0,
+              maximumFractionDigits: 0,
+            })}
           </span>
         </div>
       </Link>
-      <Link
-        href="/"
-        className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center"
-      >
-        <Image
-          src="/pngfind.com-miami-heat-logo-png-669649.png"
-          className="group-hover:scale-105 transition-transform duration-500"
-          width={350}
-          height={350}
-          quality={100}
-          alt=""
-        />
-        <div
-          className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5"
-          title="Moletom Miami Heat"
-        >
-          <span className="text-sm truncate">Moletom Miami Heat</span>
-          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-            R$350
-          </span>
-        </div>
-      </Link>
-      <Link
-        href="/"
-        className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center"
-      >
-        <Image
-          src="/pngfind.com-nike-swoosh-png-383971.png"
-          className="group-hover:scale-105 transition-transform duration-500"
-          width={350}
-          height={350}
-          quality={100}
-          alt=""
-        />
-
-        <div
-          className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5"
-          title="Camiseta Miami Heat"
-        >
-          <span className="text-sm truncate">Camiseta Miami Heat</span>
-          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-            R$199
-          </span>
-        </div>
-      </Link>
+      {otherProducts.map((product) => {
+        return (
+          <Link
+            key={product.id}
+            href={`/product/${product.slug}`}
+            className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center"
+          >
+            <Image
+              src={product.image}
+              className="group-hover:scale-105 transition-transform duration-500"
+              width={350}
+              height={350}
+              quality={100}
+              alt=""
+            />
+            <div
+              className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5"
+              title={product.title}
+            >
+              <span className="text-sm truncate">{product.title}</span>
+              <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+                {product.price.toLocaleString("pt-br", {
+                  style: "currency",
+                  currency: "BRL",
+                  minimumFractionDigits: 0,
+                  maximumFractionDigits: 0,
+                })}
+              </span>
+            </div>
+          </Link>
+        );
+      })}
     </div>
   );
 }
