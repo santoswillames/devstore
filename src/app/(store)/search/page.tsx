2614072311ec47bd8d68d221beef88603c8bbcfc
Willames Santos <62750897+santoswillames@users.noreply.github.com>
2024-04-23 10:17:18 -0300

    refactor(app seacr): fetch search products

--- src/app/(store)/search/page.tsx
+++ src/app/(store)/search/page.tsx
@@ -1,95 +1,73 @@
+import { api } from "@/data/api";
+import { Product } from "@/data/types/product";
 import Image from "next/image";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
-export default async function Search() {
+interface SearchProps {
+  searchParams: {
+    q: string;
+  };
+}
+
+async function searchProducts(query: string): Promise<Product[]> {
+  const response = await api(`/products/search?q=${query}`, {
+    next: {
+      revalidate: 60 * 60, // 1 hour
+    },
+  });
+  const products = await response.json();
+  return products;
+}
+
+export default async function Search({ searchParams }: SearchProps) {
+  const { q: query } = searchParams;
+
+  if (!query) {
+    redirect("/");
+  }
+
+  const products = await searchProducts(query);
+
   return (
     <div className="flex flex-col gap-4">
       <p className="text-sm">
-        Resultados para: <span className="font-semibold">camiseta</span>
+        Resultados para: <span className="font-semibold">{query}</span>
       </p>
 
       <div className="grid grid-cols-3 gap-6">
-        <Link
-          href={`/product/camiseta-nike-vermelha`}
-          className="group relative rounded-lg bg-zinc-900 overflow-hidden flex justify-center"
-        >
-          <Image
-            src="/pngfind.com-camisetas-png-5353213.png"
-            className="group-hover:scale-105 transition-transform duration-500"
-            width={350}
-            height={350}
-            quality={100}
-            alt=""
-          />
-          <div
-            className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5"
-            title="Camiseta Nike Vermelha"
-          >
-            <span className="text-sm truncate">Camiseta Nike Vermelha</span>
-            <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-              {Number(139).toLocaleString("pt-br", {
-                style: "currency",
-                currency: "BRL",
-                minimumFractionDigits: 0,
-                maximumFractionDigits: 0,
-              })}
-            </span>
-          </div>
-        </Link>
-        <Link
-          href={`/product/camiseta-nike-vermelha`}
-          className="group relative rounded-lg bg-zinc-900 overflow-hidden flex justify-center"
-        >
-          <Image
-            src="/pngfind.com-camisetas-png-5353213.png"
-            className="group-hover:scale-105 transition-transform duration-500"
-            width={350}
-            height={350}
-            quality={100}
-            alt=""
-          />
-          <div
-            className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5"
-            title="Camiseta Nike Vermelha"
-          >
-            <span className="text-sm truncate">Camiseta Nike Vermelha</span>
-            <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-              {Number(139).toLocaleString("pt-br", {
-                style: "currency",
-                currency: "BRL",
-                minimumFractionDigits: 0,
-                maximumFractionDigits: 0,
-              })}
-            </span>
-          </div>
-        </Link>
-        <Link
-          href={`/product/camiseta-nike-vermelha`}
-          className="group relative rounded-lg bg-zinc-900 overflow-hidden flex justify-center"
-        >
-          <Image
-            src="/pngfind.com-camisetas-png-5353213.png"
-            className="group-hover:scale-105 transition-transform duration-500"
-            width={350}
-            height={350}
-            quality={100}
-            alt=""
-          />
-          <div
-            className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5"
-            title="Camiseta Nike Vermelha"
-          >
-            <span className="text-sm truncate">Camiseta Nike Vermelha</span>
-            <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-              {Number(139).toLocaleString("pt-br", {
-                style: "currency",
-                currency: "BRL",
-                minimumFractionDigits: 0,
-                maximumFractionDigits: 0,
-              })}
-            </span>
-          </div>
-        </Link>
+        {products.map((product) => {
+          return (
+            <Link
+              key={product.id}
+              href={`/product/${product.slug}`}
+              className="group relative rounded-lg bg-zinc-900 overflow-hidden flex justify-center max-h-[470px]"
+            >
+              <Image
+                src={product.image}
+                className="group-hover:scale-105 transition-transform duration-500"
+                width={350}
+                height={350}
+                quality={100}
+                alt=""
+              />
+              <div
+                className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5"
+                title={product.title}
+              >
+                <span className="text-sm truncate">{product.title}</span>
+                <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+                  {product.price.toLocaleString("pt-br", {
+                    style: "currency",
+                    currency: "BRL",
+                    minimumFractionDigits: 0,
+                    maximumFractionDigits: 0,
+                  })}
+                </span>
+              </div>
+            </Link>
+          );
+        })}
       </div>
     </div>
   );
