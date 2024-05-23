0dad28da6babc17304ee981e141e7771d78e4328
Willames Santos <62750897+santoswillames@users.noreply.github.com>
2024-05-23 16:39:58 -0300

    test: add cypress

---
+++ cypress/support/commands.ts
@@ -0,0 +1,16 @@
+/// <reference types="cypress" />
+
+declare namespace Cypress {
+    interface Chainable {
+        searchByQuery(query: string): Chainable<void>
+    }
+}
+  
+
+// -- This is a parent command --
+Cypress.Commands.add('searchByQuery', (query: string) => { 
+    cy.visit('/')
+
+    cy.get('input[name=q]').type(query).parent('form').submit()
+ })
+
